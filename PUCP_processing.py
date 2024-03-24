dd8a8c3c2f808e4029c78778cce5fb92ed7cb24e
Carlos Francisco García Hipólito <cfgarcia1903@gmail.com>
2024-03-24 18:28:08 -0500

    Adapted previous script
    Code based on the script used for data processing of the tambo montecarlo simulation project

--- PUCP_processing.py
+++ PUCP_processing.py
@@ -1,1 +1,251 @@
-import numpy as np
+import numpy as np 
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.colors as cl
+from timeit import default_timer
+from datetime import timedelta
+from tqdm import tqdm
+import os
+import pickle
+
+
+## FUNCTIONS
+def formato_tiempo(segundos):
+    delta_tiempo = timedelta(seconds=segundos)
+    # Construye la cadena de tiempo
+    tiempo_formateado = f"({delta_tiempo.days})D ({delta_tiempo.seconds//3600})H ({(delta_tiempo.seconds//60)%60})M ({(delta_tiempo.seconds%60)})S"
+    return tiempo_formateado
+def imprimir_barra_de_carga(tiempo,iteracion, total, longitud=50):
+    porcentaje = int(iteracion / total * 100)
+    carga = int(iteracion / total * longitud)
+    tiempo_promedio=tiempo/iteracion
+    tiempo_faltante=(tiempo_promedio*(total-iteracion))
+    barra_de_carga = f"[{'■' * carga}{' ' * (longitud - carga)}] {porcentaje}%            REMAINING TIME: {formato_tiempo(segundos=tiempo_faltante)}"
+    print(barra_de_carga, end='\r', flush=True)
+def txt_to_df(path,xlims=None,ylims=None,inclined=True):
+    # Lists to save the data
+    ids = []
+    x_values = []
+    y_values = []
+    t_values = []
+    px_values = []
+    py_values = []
+    pz_values = []
+    ek_values = []
+    w_values = []
+    lev_values = []
+
+    # accessing the .txt
+    with open(path, 'r') as archivo:
+        for linea in archivo:
+            try:
+                # Divide la línea en partes usando el espacio como separador
+                partes = linea.split()
+
+                # Extrae los valores que contienen 'x=', 'y=', 't=', etc.
+                id_valor = int(partes[1])
+                x_valor = float(partes[2].split('=')[1])/(100)   #en kilometros
+                y_valor = float(partes[3].split('=')[1])/(100)   #en kilometros          
+                t_valor = float(partes[4].split('=')[1])
+                px_valor = float(partes[5].split('=')[1])
+                py_valor = float(partes[6].split('=')[1])
+                pz_valor = float(partes[7].split('=')[1])
+                if inclined==True:
+                    x_valor,y_valor= (-y_valor),x_valor
+                    px_valor,py_valor= (-py_valor),px_valor
+                    pz_valor=-pz_valor
+                    #Now Y means upwards the inclined plane and X means to the right 
+                ek_valor = float(partes[8].split('=')[1])
+                w_valor = float(partes[9].split('=')[1])
+                lev_valor = int(partes[10].split('=')[1])
+
+                #if (det_X_inf<=x_valor<=det_X_sup) and (det_Y_inf<=y_valor<=det_Y_sup):
+                    # Agrega los valores a las listas
+                ids.append(id_valor)
+                x_values.append(x_valor)
+                y_values.append(y_valor)
+                t_values.append(t_valor)
+                px_values.append(px_valor)
+                py_values.append(py_valor)
+                pz_values.append(pz_valor)
+                ek_values.append(ek_valor)
+                w_values.append(w_valor)
+                lev_values.append(lev_valor)
+            except:
+                pass
+
+    # Crea un DataFrame de Pandas
+    data = {
+        'id': ids,
+        'x': x_values,
+        'y': y_values,
+        't': t_values,
+        'px': px_values,
+        'py': py_values,
+        'pz': pz_values,
+        'ek': ek_values,
+        'w': w_values,
+        'lev': lev_values,
+        'detector': np.nan
+    }
+
+    all_data = pd.DataFrame(data).astype({'detector':object})
+    if xlims != None:
+        all_data = all_data[(all_data['x']>= xlims[0]) & (all_data['x']<= xlims[1])].reset_index(drop=True)
+    if ylims != None:
+        all_data = all_data[(all_data['y']>= ylims[0]) & (all_data['y']<= ylims[1])].reset_index(drop=True)
+    
+    return all_data
+
+    '''
+    given a detector position and a tolerance (radius), assign_to_detector(det_position,df,tol) filters the particles that fall
+    inside that given detector and updates the dataframe of particles, assigning the
+    detector position to the 'detector' column of those entries that fall inside the detector
+    
+    it also deletes the entries that are in the neighbourhood of the detector but do not fall inside the detector
+    
+    the parameters are:
+    det_position:              a tuple that contains the position (x,y) of the detector
+    df:                        the DataFrame of all entries
+    tol:                       a tolerance for particle detection (radius of the detector)
+    pf_tol=(pf_tolx,pf_toly):  [IGNORE] a tolerance for a preliminary filtering of particles in a rectangular neighbourhood of the detector 
+                               (dimensions: 2*pf_tolx by 2*pf_toly) centered at the detector.
+                               it is necesary that tol<=pf_tol(both components). large values will cause problems if the rectangular
+                               neighbourhood is too big and overlaps with the bounds of other detectors 
+    
+    the function returns the updated DataFrame
+                          
+    '''
+    if pf_tol==(None,None):
+        pf_tol=(1.01*tol,1.01*tol)
+    
+    det_x,det_y=det_position
+    pf_tolx,pf_toly=pf_tol
+    possible_particles_index=df.index[(df['x']<=det_x+pf_tolx) & (df['x']>=det_x-pf_tolx) & (df['y']<=det_y+pf_toly) & (df['y']>=det_y-pf_toly)].tolist()
+    for index in possible_particles_index:
+        x,y=df.loc[index,'x'],df.loc[index,'y']
+        if (x-det_x)**2 + (y-det_y)**2 <= tol**2:
+            df.at[index,'detector']= det_position
+        else:
+            df.drop(index, inplace=True, axis=0)
+    return df    
+
+### FUNCIÓN DE FIORELLA
+
+
+### LISTA DE PARTÍCULAS DE JP
+
+
+### FUNCIÓN DE JD
+def assign_to_detector2(det_position,df,d_side=1):
+    '''
+    given a detector position and a tolerance (radius), assign_to_detector(det_position,df,tol) filters the particles that fall
+    inside that given detector and updates the dataframe of particles, assigning the
+    detector position to the 'detector' column of those entries that fall inside the detector
+    
+    it also deletes the entries that are in the neighbourhood of the detector but do not fall inside the detector
+    
+    the parameters are:
+    det_position:              a tuple that contains the position (x,y) of the detector
+    df:                        the DataFrame of all entries
+    tol:                       a tolerance for particle detection (radius of the detector)
+    pf_tol=(pf_tolx,pf_toly):  [IGNORE] a tolerance for a preliminary filtering of particles in a rectangular neighbourhood of the detector 
+                               (dimensions: 2*pf_tolx by 2*pf_toly) centered at the detector.
+                               it is necesary that tol<=pf_tol(both components). large values will cause problems if the rectangular
+                               neighbourhood is too big and overlaps with the bounds of other detectors 
+    
+    the function returns the updated DataFrame
+                          
+    '''
+        
+    det_x,det_y=det_position
+    possible_particles_index=df.index[(df['x']<=det_x+d_side/2.0) & (df['x']>=det_x-d_side/2.0) & (df['y']<=det_y+d_side/2.0) & (df['y']>=det_y-d_side/2.0)].tolist()
+    for index in possible_particles_index:
+        df.at[index,'detector']= det_position
+    return df    
+
+###
+
+def list_directories(path):
+    directories = []
+    for name in os.listdir(path):
+        full_path = os.path.join(path, name)
+        if os.path.isdir(full_path) and not name[0]=='.':
+            directories.append(name)
+    return directories
+def list_dats(path):
+    directories = []
+    for name in os.listdir(path):
+        full_path = os.path.join(path, name)
+        if os.path.isdir(full_path):
+            directories.append(name)
+    return directories
+
+
+
+## PROCESS_DATA FUNCTION
+def process_data(txt_path):
+    ## TXT to Dataframe
+    all_particles_df=txt_to_df(txt_path,xlims=xlims,ylims=ylims,inclined=False)  ## edit limits
+    
+    ## add Fiorella's and JD's Functions
+
+    return None ## edit return
+    
+
+## PARAMETERS
+# Paths
+# txt_path='8-12/DAT000008-inclined (2).txt'
+
+
+data_directory = r'C:\Users\cg_h2\Documents\data_tambo\DATA'
+energy_directories = list_directories(data_directory)
+
+primaries_list=[]
+det_energies_list=[]
+det_totals_list=[]
+exceptions=[]
+count=0
+for directory_name in energy_directories:
+    print(f'{directory_name} is being processed.')
+    primary_energy=float(directory_name.split('_')[1])
+    directory_path= os.path.join(data_directory, directory_name)
+    try:
+        txt_path= os.path.join(directory_path, 'data01.txt')
+        det_total_energy,det_energies = process_data(txt_path,detector_grid_list,complete_grid_list)
+        primaries_list.append(primary_energy)
+        det_energies_list.append(det_energies)
+        det_totals_list.append(det_total_energy)        
+    except:
+        print(f'\n{directory_name} Failed.')
+        exceptions.append(directory_name)
+
+    print(f'\n{directory_name} successful.')
+    count+=1
+    left=len(energy_directories)-count
+    print(f'{left} directories remaining')
+print('Data has been processed')
+print(f'The following exceptions have been encountered{exceptions}.')
+
+if len(primaries_list)==len(det_energies_list) and len(det_energies_list)== len(det_totals_list):
+    save_flag = input('Save data?[y/n]: ')
+    if save_flag=='y':
+        pickle_path=r'C:\Users\cg_h2\Documents\data_tambo'
+        pimaries_path=os.path.join(pickle_path, 'primaries.pickle')
+        det_energies_path=os.path.join(pickle_path, 'det_energies.pickle')
+        det_totals_path=os.path.join(pickle_path, 'det_totals.pickle')
+
+        with open(pimaries_path, 'wb') as file:
+            pickle.dump(primaries_list, file)
+
+        with open(det_energies_path, 'wb') as file:
+            pickle.dump(det_energies_list, file)
+
+        with open(det_totals_path, 'wb') as file:
+            pickle.dump(det_totals_list, file)
+    else:
+        print('Not saved')
+else:
+    print('List lengths don\'t match, please debug')
+
+
